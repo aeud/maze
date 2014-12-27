0096c99c8cd255457bf79ab16d082b6b8e1da3b8
aeud <adrien.eudes@gmail.com>
2014-12-27 22:26:00 +0800

    Begin the script

--- app.js
+++ app.js
@@ -3,11 +3,35 @@ var n = 2
 
 var matrix = [];
 
+var places = [];
+
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 for (var i = 0; i < m; i++) {
 	matrix[i] = [];
 	for (var j = 0; j < n; j++) {
 		matrix[i][j] = null;
 	}
+}
+
+var init = {
+	x: 0,
+	y: 0,
+	n: false,
+	e: true,
+	s: true,
+	w: false,
+	prev: null
+};
+matrix[0][0] = init;
+places.push(init);
+
+while (places.length > 0) {
+	var pop = places.pop();
+	var r = rand(0,3);
+	
 }
 
 console.log(matrix);
